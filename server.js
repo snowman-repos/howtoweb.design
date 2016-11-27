abf5958afb073d8b2abaa65707d65eb7dd81e88c
Darryl Snow <darryl@yourweb.expert>
2016-11-28 03:28:52 +0800

    update server

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.post('/charge', function(req, res){
   var stripeToken = req.body.stripeToken;
 
   var charge = stripe.charges.create({
-    amount: 500,
+    amount: 9700,
     currency: 'usd',
     card: stripeToken,
     description: 'How to web design donation'
